49215931b89374603410b5186ae67e2dfa88961d
SGBD <68038556+SGBDr@users.noreply.github.com>
2022-03-09 00:16:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/UI_UX'));
+app.use(express.static(__dirname + '/dist/CodeJs'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/UI_UX/index.html'));});
